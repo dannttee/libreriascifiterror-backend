8780bbd10e32689f084fed1c78bdebd8083111ec
Dannttee <dant.munozf@duocuc.cl>
2025-11-27 04:04:01 -0300

    Agregar Maven Wrapper y configuración de proyecto

---
+++ src/main/java/com/libreriasfi/LibreriasfiterrorBackendApplication.java
@@ -0,0 +1,13 @@
+package com.libreriasfi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibreriasfiterrorBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LibreriasfiterrorBackendApplication.class, args);
+    }
+
+}
