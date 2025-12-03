0a513809d4b7f1f54e55e083930d136fb6dcde30
dannttee <dant.munozf@duocuc.cl>
2025-12-02 21:19:06 -0300

    actualización de CorsConfig

--- src/main/java/com/libreriasfi/config/CorsConfig.java
+++ src/main/java/com/libreriasfi/config/CorsConfig.java
@@ -13,7 +13,8 @@ public class CorsConfig implements WebMvcConfigurer {
                 .allowedOrigins(
                         "http://localhost:5173",
                         "http://localhost:3000",
-                        "http://127.0.0.1:5173"
+                        "http://127.0.0.1:5173",
+                        "https://libreria-scifi-react.vercel.app"  
                 )
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
